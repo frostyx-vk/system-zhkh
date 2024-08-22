a243e46d051405697e4f24620b1f2ec6009808e7
frostyx-vk <vk.xam@yandex.ru>
2024-08-22 19:42:43 +0700

    setting

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+// import React, { useState, useEffect } from 'react'
+import s from './Header.module.css'
+import { Link } from 'react-router-dom'
+// import { ReactComponent as PhoneIcon } from '../../svg/phone.svg'
+// import { ReactComponent as TelegramIcon } from '../../svg/telegram.svg'
+// import logo from '../../illustration/logo.png'
+// import Nav from '../Nav/Nav'
+// import Modal from '../Modal/Modal'
+
+export default function Header() {
+    return (
+        <div className={s.header}>
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,23 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './font/stylesheet.css'
+import Header from './components/Header/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter >
-    {/* <React.StrictMode>
+    <React.StrictMode>
       <Header />
-      <Routes>
-        <Route path='/home' element={ <HomePage />} />
-        <Route path='/about' element={ <About />} />
-        <Route path='/service' element={ <Service />} />
-        <Route path='/news' element={ <News />} />
-        <Route path='/contacts' element={ <Contacts />} />
-        <Route path='*' element={ <NotFoundPage />} />
+      {/* <Routes>
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/service' element={<Service />} />
+        <Route path='/news' element={<News />} />
+        <Route path='/contacts' element={<Contacts />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
-      <Footer />
-    </React.StrictMode> */}
+      <Footer /> */}
+    </React.StrictMode>
   </ BrowserRouter>
 );
 
