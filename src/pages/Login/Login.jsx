76bfd75dbb899e888e36f0f41fa4ac328c9e93af
frostyx-vk <vk.xam@yandex.ru>
2024-08-31 16:15:29 +0700

    состояние для инпута

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './Login.module.css'
 import { Input } from '@chakra-ui/react'
 
 function Login() {
+    const [userAccount, setUserAccount] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+    
+    function handlerForm(event) {
+        event.preventDefault();
+
+        console.log('Hi')
+    }
+
     return (
         <main className='content'>
             <div className='wrapper'>
                 <div className={s.wrapperForm}>
-                    <form className={s.loginForm} method="post" action="">
-                        <Input type='text' placeholder='Введите номер Лицевого счета' size='md' name="userAccount" />
-                        <Input type='password' placeholder='Введите пароль' size='md' name="userPassword" />
-                        <Input type="submit" value="Отправить" />
+                    <h2>Войдите в личный кабинет</h2>
+                    <form onSubmit={handlerForm} className={s.loginForm} method="post" action="">
+                        <Input type='text'
+                            placeholder='Введите номер Лицевого счета'
+                            size='md'
+                            name="userAccount"
+                            onChange={e => setUserAccount(e.target.value)} />
+                        <Input type='password'
+                            placeholder='Введите пароль'
+                            size='md'
+                            name="userPassword"
+                            onChange={e => setUserPassword(e.target.value)} />
+                        <Input type="submit" value="Войти" />
                     </form>
                 </div>
             </div>
