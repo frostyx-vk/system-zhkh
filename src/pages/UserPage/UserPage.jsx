b281767f54dbd758b3c5e75cae280f190465b1e8
frostyx-vk <vk.xam@yandex.ru>
2024-09-01 15:06:26 +0700

    тестовая кнопка выхода их лк и переадресация

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function UserPage() {
+
+  let navigator = useNavigate();
+  
+  function clearAuth() {
+    localStorage.clear();
+    navigator('/login');
+  }
+
   return (
     <main className='content'>
       <div className='wrapper'>
         Страница авторизованного пользователя
+        <button onClick={() => clearAuth()} style={{marginLeft:'50px'}}>Exit</button>
       </div>
     </main>
   )
