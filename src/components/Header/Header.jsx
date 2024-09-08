63042b7b48279d847348eeafda1d34bf66e242bb
frostyx-vk <vk.xam@yandex.ru>
2024-09-08 15:46:49 +0700

    работа кнопок пользователя в хэдере

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
 // import React, { useState, useEffect } from 'react'
 import s from './Header.module.css'
 import { Link } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom';
 // import { ReactComponent as PhoneIcon } from '../../svg/phone.svg'
 // import { ReactComponent as TelegramIcon } from '../../svg/telegram.svg'
 // import logo from '../../illustration/logo.png'
 import Nav from '../Nav/Nav'
-// import Modal from '../Modal/Modal'
 
 export default function Header() {
+
+    let navigator = useNavigate();
+
+    function clearAuth() {
+        localStorage.clear();
+        navigator('/login');
+    }
+
+    const location = useLocation();
+
+    console.log(localStorage.admin)
+
     return (
         <div className={s.header}>
             <div className={s.headerWrapper}>
@@ -16,11 +28,26 @@ export default function Header() {
                 </Link>
                 <div className={s.headerNav}>
                     <Nav />
-                    <button>
-                        <Link to='/login'>
-                            Личный кабинет
-                        </Link>
-                    </button>
+                    {
+                        localStorage.admin === "admin" ?
+                            <div>
+                                <button>
+                                    <Link to='/userpage'>
+                                        Пользователь
+                                    </Link>
+                                </button>
+                                <button onClick={() => clearAuth()}>
+                                    <Link>
+                                        Выход
+                                    </Link>
+                                </button>
+                            </div> :
+                            <button>
+                                <Link to='/login'>
+                                    Личный кабинет
+                                </Link>
+                            </button>
+                    }
                 </div>
             </div>
         </div>
