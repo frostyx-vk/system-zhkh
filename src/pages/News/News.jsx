2df3bf3dbbed79a1c8e2e54b60905008104958d6
frostyx-vk <vk.xam@yandex.ru>
2024-11-19 22:45:33 +0700

    работа с новостями

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import s from './News.module.css'
 import {
-    newsList
+  newsList
 } from '../../api/index'
 
-function  News() {
+function News() {
+
   return (
     <main className='content'>
       <div className='wrapper'>{
         newsList.map((item, i) => {
-            return <div>
-                <div>{item.title}</div>
-                <div>{item.description}</div>
-                <div>{item.image}
-                    <img src={item.image} alt="Новость"/>
-                </div>
-                <div>{item.date_created}</div>
+          return <div key={i} className={s.newsBlock}>
+            <div className={s.titleBlock}>
+              <div>
+                {item.title}
+              </div>
+              <div>
+                {
+                  item.date_created.slice(0, -8).replace(/T/, ' ')
+                }
+              </div>
             </div>
+            <div className={s.newsDescriptions}>
+              {item.description}
+            </div>
+          </div>
         })
       }
       </div>
