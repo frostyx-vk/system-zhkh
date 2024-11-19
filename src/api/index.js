26c3ddc80ca4df9d7f6945d35037e9a52d7501ca
Vladimir_Filippov <v.filippov1994@gmail.com>
2024-11-19 21:46:41 +0700

    Вывод услуг

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import {
     serverBaseUrl,
-    NEWS_LIST
+    NEWS_LIST,
+    SERVICE_LIST
 } from '../api/urls'
 
-
-export const newsList = await axios.get(`${serverBaseUrl}${NEWS_LIST}`, { headers: {"Authorization" : 'Token ' + localStorage.accessToken}}).then(res => res.data).catch(err => console.log(err));
+const headers = { headers: {"Authorization" : 'Token ' + localStorage.accessToken}}
+export const newsList = await axios.get(`${serverBaseUrl}${NEWS_LIST}`, headers).then(res => res.data).catch(err => console.log(err));
+export const serviceList = await axios.get(`${serverBaseUrl}${SERVICE_LIST}`, headers).then(res => res.data).catch(err => console.log(err));
--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import {
+    serviceList
+} from '../../api/index'
 
 function Service() {
   return (
     <main className='content'>
-      <div className='wrapper'>
-        Здесь будет перечень услуг УК.
-      </div>
+        <div className='wrapper'>
+            <table className='service-table'>
+                <tr>
+                    <th></th>
+                    <th>Услуги</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th>Название</th>
+                    <th>Описание услуги</th>
+                    <th>Цена</th>
+                </tr>
+                {
+                    serviceList.map((item, i) => {
+                        return <tr>
+                            <td>{item.title}</td>
+                            <td>{item.description}</td>
+                            <td>{item.price}</td>
+                        </tr>
+                    })
+                }
+            </table>
+        </div>
     </main>
   )
 }
