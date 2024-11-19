09d6cf6f990b65199a074fb9dfed7d9c5513db42
Vladimir_Filippov <v.filippov1994@gmail.com>
2024-11-19 20:19:29 +0700

    новости

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {
+    newsList
+} from '../../api/index'
 
-function News() {
+function  News() {
   return (
     <main className='content'>
-      <div className='wrapper'>
-        Здесь будет список новостей УК.
+      <div className='wrapper'>{
+        newsList.map((item, i) => {
+            return <div>
+                {item.title}
+                {item.description}
+                {item.image}
+                {item.date_created}
+            </div>
+        })
+      }
       </div>
     </main>
   )
