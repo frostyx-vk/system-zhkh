3030477d1a0118a149990c788ac73c0ba48a19cf
frostyx-vk <vk.xam@yandex.ru>
2024-11-22 11:24:19 +0700

    реализованы табы

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './UserPage.module.css'
 import NavPersonal from '../../components/NavPersonal/NavPersonal'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 function UserPage() {
 
@@ -11,7 +12,22 @@ function UserPage() {
           <div className='personalNav'>
             <NavPersonal />
           </div>
-          <div className='personalContent'></div>
+          <div className='personalContent'>
+            <Tabs variant='soft-rounded' colorScheme='green'>
+              <TabList className={s.personalTabs}>
+                <Tab>Личная информация</Tab>
+                <Tab>Смена пароля</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel className={s.personalTabsContent}>
+                  <p>one!</p>
+                </TabPanel>
+                <TabPanel className={s.personalTabsContent}>
+                  <p>two!</p>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </div>
         </div>
       </div>
     </main>
