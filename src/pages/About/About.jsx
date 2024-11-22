10245e9162c3ea878c6d7839fc7e3c457b4267d0
frostyx-vk <vk.xam@yandex.ru>
2024-11-22 16:05:45 +0700

    Доработка О портале

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
-import s from './About.module.jsx'
+import s from './About.module.css'
 import {
     aboutPortalList,
     contactList
 } from '../../api/index'
 
 function About() {
-    console.log(aboutPortalList)
+
     return (
         <main className='content'>
             <div className='wrapper'>{
                 aboutPortalList.map((item, i) => {
-                    return <div>
-                        <h1>{item.title}</h1>
-                        <div><p>{item.description}</p></div>
-                        <div>{item.address}</div>
-                        <div>
-                            <p>{item.phone_organization}</p>
-                            <p>{item.email_organization}</p>
+                    return <div key={i}>
+                        <h1 className={s.title}>{item.title}</h1>
+                        <p className={s.description}>{item.description}</p>
+                        <div className={s.addres}>
+                            Мы находимся по адресу: {item.address}
+                            <p>Номер телефона компании: {item.phone_organization}</p>
+                            <p>E-mail адрес компании: {item.email_organization}</p>
                         </div>
                     </div>
                 })
-            } {
-                contactList.map((item, i) => {
-                    return <div>
-                        <p>{item.name}</p>
-                        <p>{item.phone}</p>
-                        <p>{item.email}</p>
-                    </div>
-                })
             }
+                {/* {
+                    contactList.map((item, i) => {
+                        return <div>
+                            <p>{item.name}</p>
+                            <p>{item.phone}</p>
+                            <p>{item.email}</p>
+                        </div>
+                    })
+                } */}
             </div>
         </main>
     )
