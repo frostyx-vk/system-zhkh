6042b8ac65c29ddd6eb78447e3040af54eace48e
Vladimir_Filippov <v.filippov1994@gmail.com>
2024-12-01 17:35:49 +0700

    личная инф-ия

--- src/api/urls.js
+++ src/api/urls.js
@@ -4,6 +4,7 @@ export let serverBaseUrl = `${serverUrl}/`;
 export const USER_LOGIN = 'auth/token/login/';
 export const RESET_PASSWORD = 'auth/users/reset_password/';
 export const LOGOUT = 'auth/token/logout/';
+export const USERPAGE_API = 'auth/users/me/';
 
 export const NEWS_LIST = 'web/news/';
 export const SERVICE_LIST = 'web/services/';
@@ -11,3 +12,4 @@ export const ABOUT_PORTAL_LIST = 'web/about-portal/';
 export const CONTACT_LIST = 'web/contacts/';
 
 export const MESSAGE_PROBLEM = 'communication/create-message-problem/';
+
