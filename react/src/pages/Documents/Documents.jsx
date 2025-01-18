7cdd44f0d01edb6dd3d3d5722f6a1b017bad7184
frostyx-vk <vk.xam@yandex.ru>
2025-01-18 21:03:32 +0700

    реализован список документов

--- react/src/pages/Documents/Documents.jsx
+++ react/src/pages/Documents/Documents.jsx
@@ -3,6 +3,7 @@ import s from './Documents.module.css'
 import NavPersonal from '../../components/NavPersonal/NavPersonal'
 import axios from "axios";
 
+import { FaRegFilePdf } from "react-icons/fa6";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -21,6 +22,25 @@ function Documents() {
       });
   }, [])
 
+  // console.log(documents)
+
+  // useEffect(() => {
+  //   const downloadFile = (documents) => {
+  //     for (const doc of documents) {
+  //       console.log(doc)
+  //       const url = URL.createObjectURL(new Blob([doc.file]), {type: "application/pdf"});
+  //       const link = document.createElement("a");
+  //       link.href = url;
+  //       link.setAttribute("download", doc.title);
+  //       document.body.appendChild(link);
+  //       link.click();
+  //       console.log(link)
+  //     }
+  //   }
+
+  //   downloadFile(documents)
+  // }, [documents])
+
   return (
     <main className={s.content}>
       <div className='wrapper'>
@@ -34,13 +54,18 @@ function Documents() {
             </div>
             <div className={s.documents}>
               <p>
-                Для информации можете скачать и ознакомиться с перечнем документов:
+                Для информации можете ознакомиться с перечнем документов:
               </p>
               <div>
                 {documents.length > 0 ? (
                   <ul>
-                    {documents.map(document => (
-                      <li key={document.id}>{document.name}</li>
+                    {documents.map((document, i) => (
+                      <li key={i}>
+                        <a download target="_blank" href={document.file}>
+                          <FaRegFilePdf />
+                          {document.title}
+                        </a>
+                      </li>
                     ))}
                   </ul>
                 ) : <p>Документы отсутствуют</p>}
