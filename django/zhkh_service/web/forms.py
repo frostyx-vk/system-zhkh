bed362c5394149a35c5e97c0b700858714c9a200
Vladimir_Filippov <v.filippov1994@gmail.com>
2025-01-19 11:59:40 +0700

    Сохранение-только-файлов-pdf

---
+++ django/zhkh_service/web/forms.py
@@ -0,0 +1,21 @@
+from xml.dom import VALIDATION_ERR
+
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Documents
+
+
+
+
+class DocumentAdminForm(forms.ModelForm):
+    class Meta:
+        model = Documents
+        fields = ('title', 'description', 'file')
+
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        pdf = str(file).split('/')[-1]
+        if pdf.split('.')[1].lower() != 'pdf':
+            raise ValidationError('Недопустимое расширение файла. Допускаются только файлы PDF')
+        return file
