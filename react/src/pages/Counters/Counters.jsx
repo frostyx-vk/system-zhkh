8fc632913e30ed07ff4cb714f57b45e7a403cde7
frostyx-vk <vk.xam@yandex.ru>
2025-01-20 21:49:37 +0700

    создана форма отправки для жилого помещения

--- react/src/pages/Counters/Counters.jsx
+++ react/src/pages/Counters/Counters.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import s from './Counters.module.css'
 import NavPersonal from '../../components/NavPersonal/NavPersonal'
 
+import { Input } from '@chakra-ui/react'
+
 function Counters() {
 
   return (
@@ -14,6 +16,36 @@ function Counters() {
           <div className='personalContent'>
             <div className={s.title}>
               Передача показаний счетчиков
+            </div>
+            <div className={s.counters}>
+              <p>C 20 по 25 число каждого месяца требуется отправлять показания счётчиков:</p>
+              <form>
+                <label>
+                  Холодная вода:
+                  <Input
+                    // value={}
+                    placeholder='Введите число'
+                    size='md'
+                  />
+                </label>
+                <label>
+                  Горячая вода:
+                  <Input
+                    // value={}
+                    placeholder='Введите число'
+                    size='md'
+                  />
+                </label>
+                <label>
+                  Электричество:
+                  <Input
+                    // value={}
+                    placeholder='Введите число'
+                    size='md'
+                  />
+                </label>
+                <button type='submit'>Отправить</button>
+              </form>
             </div>
           </div>
         </div>
