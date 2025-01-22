50c1691a515becc3ac43548a6ee2972db6b901d8
frostyx-vk <vk.xam@yandex.ru>
2025-01-22 22:08:44 +0700

    реализован запрос за информацией пользователя

--- react/src/pages/Counters/Counters.jsx
+++ react/src/pages/Counters/Counters.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import s from './Counters.module.css'
 import NavPersonal from '../../components/NavPersonal/NavPersonal'
+import axios from "axios";
 
 import { Input } from '@chakra-ui/react'
 
 function Counters() {
+  const [isParking, setIsParking] = useState(null);
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/accounts/get-user-data/',
+      { headers: { "Authorization": 'Token ' + sessionStorage.accessToken } })
+      .then(response => {
+        setUserData(response.data.data);
+        if (response.data.data.type !== "PARKING") {
+          setIsParking(false)
+        } else setIsParking(true);
+      })
+      .catch((err) => {
+        console.log(err)
+        // toast.error("Ошибка! Информация отсутствует.")
+      });
+  }, []);
 
   return (
     <main className={s.content}>
@@ -18,7 +36,10 @@ function Counters() {
               Передача показаний счетчиков
             </div>
             <div className={s.counters}>
-              <p>C 20 по 25 число каждого месяца требуется отправлять показания счётчиков:</p>
+              <p>
+                C 20 по 25 число каждого месяца требуется отправлять показания счётчиков<br /><br />
+                Имущество: {userData.type}, {userData.square}м²
+              </p>
               <form>
                 <label>
                   Холодная вода:
