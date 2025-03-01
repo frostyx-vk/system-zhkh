026cde736cbd99000719e129523050d5e83ebd01
frostyx-vk <vk.xam@yandex.ru>
2025-03-01 22:00:00 +0700

    создана ветка, добавлен селект

--- react/src/pages/Invoice/Invoice.jsx
+++ react/src/pages/Invoice/Invoice.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import s from './Invoice.module.css'
 import NavPersonal from '../../components/NavPersonal/NavPersonal'
+import axios from "axios";
+
+import { Select } from '@chakra-ui/react'
 
 function Invoice() {
+  const [invoice, setInvoice] = useState([])
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:8000/web/documents/',
+  //     { headers: { "Authorization": 'Token ' + sessionStorage.accessToken } })
+  //     .then(response => {
+  //       setDocuments(response.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //       toast.error("Ошибка! Информация недоступна, зайдите позже")
+  //     });
+  // }, [])
 
   return (
     <main className={s.content}>
@@ -14,6 +30,21 @@ function Invoice() {
           <div className='personalContent'>
             <div className={s.title}>
               Платежные документы
+            </div>
+            <div className={s.invoice}>
+              <p>
+                Выберете месяц, за который хотите получить платежный документ:
+              </p>
+              <div>
+                <Select placeholder='Select option'>
+                  <option value='option1'>Option 1</option>
+                  <option value='option2'>Option 2</option>
+                  <option value='option3'>Option 3</option>
+                </Select>
+                <button>
+                  
+                </button>
+              </div>
             </div>
           </div>
         </div>
