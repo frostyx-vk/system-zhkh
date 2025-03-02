bc7b23ba45d6eab31a6d2ebe125fd687f4776ffa
frostyx-vk <vk.xam@yandex.ru>
2025-03-02 17:10:19 +0700

    Сделан пример истории платежей

--- react/src/pages/Payments/Payments.jsx
+++ react/src/pages/Payments/Payments.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import s from './Payments.module.css'
 import NavPersonal from '../../components/NavPersonal/NavPersonal'
+import axios from "axios";
+
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react'
+
 
 function Payments() {
+  const [payments, setPayments] = useState([])
 
   return (
     <main className={s.content}>
@@ -12,9 +18,44 @@ function Payments() {
             <NavPersonal />
           </div>
           <div className='personalContent'>
-            <div className={s.title}>
-              Платежи за выбранный период
-            </div>
+            <Tabs variant='soft-rounded' colorScheme='green'>
+              <TabList className={s.personalTabs}>
+                <Tab>Оплата</Tab>
+                <Tab>История платежей</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel className={s.personalTabsContent}>
+                  <div className={s.personalTabsContent1}>
+                    <p>Здесь будет оплата</p>
+                  </div>
+                </TabPanel>
+                <TabPanel className={s.personalTabsContent}>
+                  <div className={s.personalTabsContent1}>
+                    <p>История платежей:</p>
+                    <TableContainer>
+                      <Table variant='simple'>
+                        <Thead>
+                          <Tr>
+                            <Th>Дата платежа</Th>
+                            <Th isNumeric>Сумма платежа, ₽</Th>
+                          </Tr>
+                        </Thead>
+                        <Tbody>
+                          <Tr>
+                            <Td>07.01.2025</Td>
+                            <Td isNumeric>4205,75</Td>
+                          </Tr>
+                          <Tr>
+                            <Td>10.02.2025</Td>
+                            <Td isNumeric>3842,22</Td>
+                          </Tr>
+                        </Tbody>
+                      </Table>
+                    </TableContainer>
+                  </div>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </div>
         </div>
       </div>
