0505c81e7d86432abcebe788f86ad5cc01d603b7
frostyx-vk <vk.xam@yandex.ru>
2025-03-23 16:30:30 +0700

    обработка ошибок в новостях

--- react/src/pages/News/News.jsx
+++ react/src/pages/News/News.jsx
@@ -1,54 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import s from './News.module.css'
-import { useNavigate } from 'react-router-dom';
+import { Spinner } from '@chakra-ui/react'
+import { ToastContainer, toast } from 'react-toastify';
 
 function News() {
   const [news, setNews] = useState([]);
-
-  let navigator = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios.get('http://localhost:8000/web/news/')
-      .then(res => setNews(res.data))
+      .then(res => {
+        setLoading(false);
+        setNews(res.data);
+      })
       .catch(err => {
-        if (err.response) {
-          console.log('Отсутствует ответ')
-          navigator('/404')
-        } else if (err.request) {
-          console.log('Ошибка запроса')
-        } else {
-          console.log('Другая ошибка')
-        }
+        setLoading(true);
+        console.log(err);
+        toast.error("Ошибка! Информация недоступна, зайдите позже");
       });
   }, [])
 
-  console.log(news)
-
   return (
     <main className='content'>
-      <div className='wrapper'>{
-        news.map((item, i) => {
-          return <div key={i} className={s.newsBlock}>
-            <div className={s.titleBlock}>
-              <div>
-                {item.title}
-              </div>
-              <div>
-                <span className={s.dateNews}>
-                  {item.date_created.slice(0, 10).split('-').reverse().join('-').replace(/-/g, '.')}
-                </span>
-                <span>
-                  {item.date_created.slice(11, 19)}
-                </span>
-              </div>
+      <div className='wrapper'>
+        {
+          loading ?
+            <div className={s.spinner}>
+              <Spinner size='lg' color='green.500' />
             </div>
-            <div className={s.newsDescriptions}>
-              {item.description}
-            </div>
-          </div>
-        })
-      }
+            :
+            <>
+              {
+                news.map((item, i) => {
+                  return <div key={i} className={s.newsBlock}>
+                    <div className={s.titleBlock}>
+                      <div>
+                        {item.title}
+                      </div>
+                      <div>
+                        <span className={s.dateNews}>
+                          {item.date_created.slice(0, 10).split('-').reverse().join('-').replace(/-/g, '.')}
+                        </span>
+                        <span>
+                          {item.date_created.slice(11, 19)}
+                        </span>
+                      </div>
+                    </div>
+                    <div className={s.newsDescriptions}>
+                      {item.description}
+                    </div>
+                  </div>
+                })
+              }
+            </>
+        }
+        <ToastContainer position="top-right" />
       </div>
     </main>
   )
